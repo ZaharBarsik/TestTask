7aca90eca680d5f7e564aa5bf5b4d1c52594da80
PC <deadp7408@gmail.com>
2024-05-06 22:35:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine(comp(x));
+        }
+
+        private static string comp(int x)
+        {
+            int y = x % 10;
+            if ((x >= 11) && (x <= 20))
+            {
+                return(x + " компьютеров");
+            }
+            switch (y)
+            {
+                case 1:
+                    return (x + " компьютер");
+                case 2 or 3 or 4:
+                    return (x + " компьютера");
+                case 0 or 5 or 6 or 7 or 8 or 9:
+                    return (x + " компьютеров");
+            }
+            throw new Exception("Ашибка");
+        }
+    }
+}
